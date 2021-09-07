d290eea1ee7ff911ad96995ffb5f4736cd612afd
salu pc <salurobles@gmail.com>
2021-09-06 22:32:06 -0300

    Agregando tipografia y corrigiendo detalles

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { CreateTodoButton } from './CreateTodoButton';
 //import './App.css';
 
 const todos = [
-  { text: 'Cortar cebolla', completed: false },
+  { text: 'Cortar cebolla', completed: true },
   { text: 'Tomar  el curso de intro a React', completed: false },
   { text: 'Llorar con la llorina', completed: false }
 ]
@@ -21,7 +21,7 @@ function App() {
 
     <TodoList>
       {todos.map(todo => (
-        <TodoItem key = {todo.text} text = {todo.text}/>
+        <TodoItem key = {todo.text} text = {todo.text} completed = {todo.completed}/>
       ))}
       </TodoList>
 
