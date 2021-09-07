1cdb4458d5671b0789724f1e98c44e2328bc88f3
salu pc <salurobles@gmail.com>
2021-09-07 17:44:46 -0300

    Manejo de eventos

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,8 +2,15 @@ import React from "react";
 import './CreateTodoButton.css';
 
 function CreateTodoButton(props) {
+
+    const onClickButton = (msg) => {
+        alert(msg);
+    };
     return (
-        <button className = "CreateTodoButton">+</button>
+        <button className = "CreateTodoButton"
+        onClick = {() => onClickButton('Aqui se deberia abrir el modal')}>
+            +
+        </button>
     );
 }
 
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,15 +2,26 @@ import React from 'react';
 import './TodoItem.css';
 
 function TodoItem (props) {
+    const onCompleted = () => {
+        alert('Ya completaste el todo ' + props.text);
+    };
+
+    const onDelete = () => {
+        alert('Borraste el todo ' + props.text)
+    }
     return (
         <li className = "TodoItem">
-            <span className = {`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
+            <span className = {`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+            onClick = {onCompleted}
+            >
                 √
             </span>   
             <p className = {`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
                 {props.text}
             </p>
-            <spam className = "Icon Icon-delete">
+            <spam className = "Icon Icon-delete"
+            onClick = {onDelete}
+            >
                 X
             </spam>
 
