160bd8eb60723dcdfe80bb98c6d726f8071c1a1c
salu pc <salurobles@gmail.com>
2021-09-07 18:13:23 -0300

    Manejo de estados

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -3,16 +3,22 @@ import './TodoSearch.css';
 
 
 function TodoSearch () {
+
+    const [searchValue, setSearchValue] = React.useState('');
+
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value);
     }
-    return (
+    return [
         <input 
         className = "TodoSearch" 
         placeholder = "Cebolla"
+        value = { searchValue }
         onChange={onSearchValueChange}
-        />
-    );
+        />,
+        <p>{ searchValue }</p>
+    ]
 }
 
 export {TodoSearch};
